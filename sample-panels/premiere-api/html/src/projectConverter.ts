6c53e31e6e73bb5c6f7a9a81d7e6b949afeef6f0
Cameron Legleiter <camlegleiter@gmail.com>
2026-04-13 09:04:43 -0500

    Fix linter warnings for projectConverter.ts

--- sample-panels/premiere-api/html/src/projectConverter.ts
+++ sample-panels/premiere-api/html/src/projectConverter.ts
@@ -14,6 +14,7 @@
 
 import type { premierepro, Sequence } from "../types.d.ts";
 import { log } from "./utils";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const ppro = require("premierepro") as premierepro;
 
 /**
